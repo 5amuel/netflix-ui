196174bd0776f2aea801ca29507a4728b7108c1f
Samuel Martinez <samuel.martinez.morales@gmail.com>
2022-11-07 21:23:22 -0600

    fetching movies from TMDB API

--- src/pages/Netflix.jsx
+++ src/pages/Netflix.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import backgroundImage from "../assets/home.jpg";
 import MovieLogo from "../assets/homeTitle.webp";
@@ -6,15 +6,30 @@ import { FaPlay } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import styled from "styled-components";
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies, getGenres } from "../store";
 
 
 const Netflix = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+
+    const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+    const movies = useSelector((state) => state.netflix.movies)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+       dispatch(getGenres())
+    }, [dispatch]);
+
+    useEffect(() => {
+        if( genresLoaded ) dispatch(fetchMovies({ type: "all" }))
+     }, [genresLoaded, dispatch]);
+    console.log(movies)
     window.onscroll = () => {
         setIsScrolled(window.pageYOffset === 0 ? false : true);
         return () => (window.onscroll = null)
     }
-    
+
     const navigate = useNavigate()
 
     return ( 
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "4dad2599de9a9d276265cf2b203bbc9e";
+export const TMBD_BASE_URL = "http://api.themoviedb.org/3";
